07158ce8e9df38d4610ec63db9475901db966575
Kingsley Brown <kingsleybrownjr@yahoo.com>
2021-05-18 15:46:04 -0500

    writtent code for the window to listen to changes

--- notes-edit.js
+++ notes-edit.js
@@ -4,9 +4,9 @@ const saveNoteBtn = document.querySelector("#save-note");
 const removeBtn = document.querySelector("#remove-note");
 
 const noteId = location.hash.substring(1);
-const notes = getSavedNotes();
+let notes = getSavedNotes();
 
-const note = notes.find(note => note.id === noteId);
+let note = notes.find(note => note.id === noteId);
 
 if (note === undefined) {
 	location.assign("/index.html");
@@ -30,4 +30,19 @@ saveNoteBtn.addEventListener("click", e => saveNotes(notes));
 removeBtn.addEventListener("click", e => {
 	removeNote(note.id);
 	saveNotes(notes);
+});
+
+window.addEventListener("storage", e => {
+	if (e.key === "notes") {
+		notes = JSON.parse(e.newValue);
+
+		note = notes.find(note => note.id === noteId);
+
+		if (note === undefined) {
+			location.assign("/index.html");
+		}
+
+		titleElement.value = note.title;
+		bodyElement.value = note.body;
+	}
 });
